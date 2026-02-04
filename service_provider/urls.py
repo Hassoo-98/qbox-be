37280f09eb374e1a4bef85b6ffa5bd3b923e5d94
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-04 13:26:28 +0500

    Create urls.py

---
+++ service_provider/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ServiceProviderListCreateView, ServiceProviderDetailView, ServiceProviderApprovalView
+
+urlpatterns = [
+    path('', ServiceProviderListCreateView.as_view(), name='service-provider-list-create'),
+    path('<int:pk>/', ServiceProviderDetailView.as_view(), name='service-provider-detail'),
+    path('<int:pk>/approve/', ServiceProviderApprovalView.as_view(), name='service-provider-approve'),
+]
