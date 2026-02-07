065af4dacb93b9ae8ba12f9eb1db8b2821123583
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-07 09:54:28 +0500

    Update urls.py

--- staff/urls.py
+++ staff/urls.py
@@ -9,10 +9,10 @@ from .views import (
 )
 
 urlpatterns = [
-    path("/", StaffListAPIView.as_view(), name="staff-list"),
-    path("/create", StaffCreateAPIView.as_view(), name="create-staff"),
-    path("/<int:id>", StaffDetailAPIView.as_view(), name="staff-detail"),
-    path("/<int:id>/update", StaffUpdateAPIView.as_view(), name="staff-update"),
-    path("/<int:id>/delete", StaffDeleteAPIView.as_view(), name="staff-delete"),
-    path("/<int:id>/change-status", StaffStatusUpdateAPIView.as_view(), name="staff-change-status"),
+    path("", StaffListAPIView.as_view(), name="staff-list"),
+    path("create", StaffCreateAPIView.as_view(), name="create-staff"),
+    path("<int:id>", StaffDetailAPIView.as_view(), name="staff-detail"),
+    path("<int:id>/update", StaffUpdateAPIView.as_view(), name="staff-update"),
+    path("<int:id>/delete", StaffDeleteAPIView.as_view(), name="staff-delete"),
+    path("<int:id>/change-status", StaffStatusUpdateAPIView.as_view(), name="staff-change-status"),
 ]
