2eb0a4719c69e5af0871d2e4b1c6757c52303de4
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-07 09:53:57 +0500

    Update urls.py

--- home_owner/urls.py
+++ home_owner/urls.py
@@ -10,12 +10,12 @@ from .views import (
     HomeOwnerResetPasswordView,
 )
 urlpatterns = [
-    path("/login", HomeOwnerLoginView.as_view(), name="homeowner-login"),
-    path("/reset-password", HomeOwnerResetPasswordView.as_view(), name="homeowner-reset-password"),
-    path("/", HomeOwnerListAPIView.as_view(), name="homeowner-list"),
-    path("/create", HomeOwnerCreateAPIView.as_view(), name="homeowner-create"),
-    path("/<uuid:id>", HomeOwnerDetailAPIView.as_view(), name="homeowner-detail"),
-    path("/<uuid:id>/update", HomeOwnerUpdateAPIView.as_view(), name="homeowner-update"),
-    path("/<uuid:id>/status", HomeOwnerStatusUpdateAPIView.as_view(), name="homeowner-status"),
-    path("/<uuid:id>/delete", HomeOwnerDeleteAPIView.as_view(), name="homeowner-delete"),
+    path("login", HomeOwnerLoginView.as_view(), name="homeowner-login"),
+    path("reset-password", HomeOwnerResetPasswordView.as_view(), name="homeowner-reset-password"),
+    path("", HomeOwnerListAPIView.as_view(), name="homeowner-list"),
+    path("create", HomeOwnerCreateAPIView.as_view(), name="homeowner-create"),
+    path("<uuid:id>", HomeOwnerDetailAPIView.as_view(), name="homeowner-detail"),
+    path("<uuid:id>/update", HomeOwnerUpdateAPIView.as_view(), name="homeowner-update"),
+    path("<uuid:id>/status", HomeOwnerStatusUpdateAPIView.as_view(), name="homeowner-status"),
+    path("<uuid:id>/delete", HomeOwnerDeleteAPIView.as_view(), name="homeowner-delete"),
 ]
