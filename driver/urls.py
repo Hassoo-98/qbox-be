b47c91ca3f6fe17ab52685ab4e971809be21df96
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-07 16:56:26 +0500

    Update urls.py

--- driver/urls.py
+++ driver/urls.py
@@ -11,8 +11,8 @@ from .views import (
 urlpatterns=[
     path("",DriverListAPIView.as_view(),name="driver-list"),
     path("create",DriverCreateAPIView.as_view(),name="driver-create"),
-    path("<str:id>/update",DriverUpdateAPIView.as_view(),name="driver-update"),
-    path("<str:id>/delete",DriverDeleteAPIView.as_view(),name="driver-delete"),
-    path("<str:id>/change-status",DriverStatusUpdateAPIView.as_view(),name="driver-change-status"),
-   path("<str:id>",DriverDetailAPIView.as_view(),name="driver-list-single")
+    path("<uuid:id>/update",DriverUpdateAPIView.as_view(),name="driver-update"),
+    path("<uuid:id>/delete",DriverDeleteAPIView.as_view(),name="driver-delete"),
+    path("<uuid:id>/change-status",DriverStatusUpdateAPIView.as_view(),name="driver-change-status"),
+   path("<uuid:id>",DriverDetailAPIView.as_view(),name="driver-list-single")
 ]
