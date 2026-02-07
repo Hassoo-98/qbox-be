ffadbf5c9b424bcdaec9cdac3c5b5ca55f8512c9
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-07 09:53:59 +0500

    Update urls.py

--- locations/urls.py
+++ locations/urls.py
@@ -16,17 +16,17 @@ from .views import (
 
 urlpatterns = [
     # City URLs
-    path('/city', CityListAPIView.as_view(), name='city-list'),
-    path('/city/create', CityCreateAPIView.as_view(), name='city-create'),
-    path('/city/<int:id>', CityDetailAPIView.as_view(), name='city-detail'),
-    path('/city/<int:id>/update', CityUpdateAPIView.as_view(), name='city-update'),
-    path('/city/<int:id>/change-status', CityStatusUpdateAPIView.as_view(), name='city-status'),
-    path('/city/<int:id>/delete', CityDeleteAPIView.as_view(), name='city-delete'),
+    path('city', CityListAPIView.as_view(), name='city-list'),
+    path('city/create', CityCreateAPIView.as_view(), name='city-create'),
+    path('city/<int:id>', CityDetailAPIView.as_view(), name='city-detail'),
+    path('city/<int:id>/update', CityUpdateAPIView.as_view(), name='city-update'),
+    path('city/<int:id>/change-status', CityStatusUpdateAPIView.as_view(), name='city-status'),
+    path('city/<int:id>/delete', CityDeleteAPIView.as_view(), name='city-delete'),
     # Area URLs
-    path('/area', AreaListAPIView.as_view(), name='area-list'),
-    path('/area/create', AreaCreateAPIView.as_view(), name='area-create'),
-    path('/area/<int:id>', AreaDetailAPIView.as_view(), name='area-detail'),
-    path('/area/<int:id>/update', AreaUpdateAPIView.as_view(), name='area-update'),
-    path('/area/<int:id>/change-status', AreaStatusUpdateAPIView.as_view(), name='area-status'),
-    path('/area/<int:id>/delete', AreaDeleteAPIView.as_view(), name='area-delete'),
+    path('area', AreaListAPIView.as_view(), name='area-list'),
+    path('area/create', AreaCreateAPIView.as_view(), name='area-create'),
+    path('area/<int:id>', AreaDetailAPIView.as_view(), name='area-detail'),
+    path('area/<int:id>/update', AreaUpdateAPIView.as_view(), name='area-update'),
+    path('area/<int:id>/change-status', AreaStatusUpdateAPIView.as_view(), name='area-status'),
+    path('area/<int:id>/delete', AreaDeleteAPIView.as_view(), name='area-delete'),
 ]
