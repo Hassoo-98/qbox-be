c9b27854acb11840cdbede2c2560439cd5adaeb7
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-07 09:54:07 +0500

    Update urls.py

--- packages/urls.py
+++ packages/urls.py
@@ -8,10 +8,10 @@ from .views import (
     PackageDeleteAPIView
 )
 urlpatterns = [
-    path('/', PackageListAPIView.as_view(), name='package-list'),
-    path('/create', PackageCreateAPIView.as_view(), name='package-create'),
-    path('/<uuid:id>', PackageDetailAPIView.as_view(), name='package-detail'),
-    path('/<uuid:id>/update', PackageUpdateAPIView.as_view(), name='package-update'),
-    path('/<uuid:id>/change-status', PackageStatusUpdateAPIView.as_view(), name='package-status'),
-    path('/<uuid:id>/delete', PackageDeleteAPIView.as_view(), name='package-delete'),
+    path('', PackageListAPIView.as_view(), name='package-list'),
+    path('create', PackageCreateAPIView.as_view(), name='package-create'),
+    path('<uuid:id>', PackageDetailAPIView.as_view(), name='package-detail'),
+    path('<uuid:id>/update', PackageUpdateAPIView.as_view(), name='package-update'),
+    path('<uuid:id>/change-status', PackageStatusUpdateAPIView.as_view(), name='package-status'),
+    path('<uuid:id>/delete', PackageDeleteAPIView.as_view(), name='package-delete'),
 ]
