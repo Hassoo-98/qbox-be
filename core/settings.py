9f2cdcf11311eb868593002d1bb4cdd52b85f08a
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-07 12:31:51 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,21 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://backend.qbox.sa"
 ]
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "{levelname} {asctime} {module} {message}", "style": "{"}
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "verbose"}
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "INFO"},
+        "django.request": {"handlers": ["console"], "level": "INFO", "propagate": True},
+    },
+}
+
 
 
 # CORS Settings
