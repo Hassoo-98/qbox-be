fa200d87b826de508c77755d8c3540a52b332134
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-12 13:37:08 +0500

    Update urls.py

--- package_timeline/urls.py
+++ package_timeline/urls.py
@@ -7,5 +7,5 @@ from .views import (
 urlpatterns=[
     path("",PackageTimeListItemView.as_view(),name="package-timeline-list"),
     path("<str:pk>",PackageTimelineDetailView.as_view(),name="package-timeline-details"),
-    path("package/<str:package_id>",PackageTimelineByPackageIdView.as_view(),name="time-by-package-id")
+    path("<str:package_id>",PackageTimelineByPackageIdView.as_view(),name="time-by-package-id")
 ]
