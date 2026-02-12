42f0fef9e2799f63caa4c07208a75ee8fa5f892e
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-12 14:30:18 +0500

    Update models.py

--- package_timeline/models.py
+++ package_timeline/models.py
@@ -14,7 +14,6 @@ class PackageTimeline(models.Model):
         verbose_name=_("Date and Time"))
     status=models.CharField(max_length=100,verbose_name=_("Status"), blank=True)
     description=models.TextField(verbose_name=_("Description"), blank=True)
-    city=models.CharField(max_length=100,verbose_name=_("City"), blank=True)
     issue_related_to=models.CharField(max_length=100,verbose_name=_("Issue Related To"), blank=True)
     package=models.ForeignKey(
         Package,
@@ -26,6 +25,6 @@ class PackageTimeline(models.Model):
      ordering=["-date_and_time"]
      verbose_name=_("Package Timeline")
      verbose_name_plural=_("Package Timelines")
-    
+     
     def __str__(self):
         return f"{self.status} - {self.package}"
