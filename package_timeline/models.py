79a58536b4dfcdc0f55cfcaa7600dbf1b0870001
Hassaan_98 <hassaanxd8@gmail.com>
2026-02-12 12:09:02 +0500

    Create models.py

---
+++ package_timeline/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from packages.models import Package
+
+class PackageTimeline(models.Model):
+    date_and_time=models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date and Time"))
+    status=models.CharField(max_length=100,verbose_name=_("Status"))
+    description=models.TextField(verbose_name=_("Description"))
+    city=models.CharField(max_length=100,verbose_name=_("City"))
+    package=models.ForeignKey(
+        Package,
+        on_delete=models.CASADE,
+        related_name="timeline",
+        verbose_name=("Package")
+    )
+    class Meta:
+     ordering=["-date_and_time"]
+     verbose_name=_("Package Timeline")
+     verbose_name_plural=_("Package Timelines")
+  
